9685dff8dea2943b9bcb428b3af0d6332c76c7a5
david <david.esteban@clouddistrict.com>
2021-07-14 14:29:27 +0200

    añadia latirud y longitud

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ window.onload = () => {
   navigator.geolocation.getCurrentPosition(function (position) {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
-    console.log("HOLAS", lat, lng);
     let places = staticLoadPlaces();
+    console.log("HOLAS", places);
     renderPlaces(places);
   });
 };
@@ -17,55 +17,14 @@ window.onload = () => {
 function staticLoadPlaces() {
   return [
     {
-      name: "Pokèmon",
+      name: "Magnemite",
       location: {
-        lat,
-        lng,
+        lat: lat,
+        lng: lng,
       },
     },
   ];
 }
-
-var models = [
-  {
-    url: "./assets/magnemite/scene.gltf",
-    scale: "0.5 0.5 0.5",
-    info: "Magnemite, Lv. 5, HP 10/10",
-    rotation: "0 180 0",
-  },
-  {
-    url: "./assets/articuno/scene.gltf",
-    scale: "0.2 0.2 0.2",
-    rotation: "0 180 0",
-    info: "Articuno, Lv. 80, HP 100/100",
-  },
-  {
-    url: "./assets/dragonite/scene.gltf",
-    scale: "0.08 0.08 0.08",
-    rotation: "0 180 0",
-    info: "Dragonite, Lv. 99, HP 150/150",
-  },
-];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-  if (model.scale) {
-    entity.setAttribute("scale", model.scale);
-  }
-
-  if (model.rotation) {
-    entity.setAttribute("rotation", model.rotation);
-  }
-
-  if (model.position) {
-    entity.setAttribute("position", model.position);
-  }
-
-  entity.setAttribute("gltf-model", model.url);
-
-  const div = document.querySelector(".instructions");
-  div.innerText = model.info;
-};
 
 function renderPlaces(places) {
   let scene = document.querySelector("a-scene");
@@ -79,19 +38,14 @@ function renderPlaces(places) {
       "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
-
-    setModel(models[modelIndex], model);
-
+    model.setAttribute("gltf-model", "./assets/magnemite/scene.gltf");
+    model.setAttribute("rotation", "0 180 0");
     model.setAttribute("animation-mixer", "");
+    model.setAttribute("scale", "0.5 0.5 0.5");
 
-    document
-      .querySelector('button[data-action="change"]')
-      .addEventListener("click", function () {
-        var entity = document.querySelector("[gps-entity-place]");
-        modelIndex++;
-        var newIndex = modelIndex % models.length;
-        setModel(models[newIndex], entity);
-      });
+    model.addEventListener("loaded", () => {
+      window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
+    });
 
     scene.appendChild(model);
   });
