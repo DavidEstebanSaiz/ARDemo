2b56a5794ec757d031ec9dc6d4de642ca026b311
david <david.esteban@clouddistrict.com>
2021-07-15 10:43:51 +0200

    debugger

--- script.js
+++ script.js
@@ -2,75 +2,72 @@ let lat = null;
 let lng = null;
 
 window.onload = () => {
-  //DECLARAR LEMENTOS
-  navigator.geolocation.getCurrentPosition(function (position) {
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-  });
+  console.log("HOLASCOMPADRE");
+  var camera = document.getElementById("camera");
+  const crd_longitude = document.getElementById("crd_longitude");
+  const crd_latitude = document.getElementById("crd_latitude");
+  const zero_crd_longitude = document.getElementById("zero_crd_longitude");
+  const zero_crd_latitude = document.getElementById("zero_crd_latitude");
+  const camera_p_x = document.getElementById("camera_p_x");
+  const camera_p_z = document.getElementById("camera_p_z");
+  const compass_heading = document.getElementById("compass_heading");
+  const camera_angle = document.getElementById("camera_angle");
+  const geohash_7chars = document.getElementById("geohash_7chars");
 
-  //DEBUG CAMARA
+  console.log("HOLAS", camera);
 
-  window.onload = function () {
-    var camera = document.getElementById("camera");
-    const crd_longitude = document.getElementById("crd_longitude");
-    const crd_latitude = document.getElementById("crd_latitude");
-    const zero_crd_longitude = document.getElementById("zero_crd_longitude");
-    const zero_crd_latitude = document.getElementById("zero_crd_latitude");
-    const camera_p_x = document.getElementById("camera_p_x");
-    const camera_p_z = document.getElementById("camera_p_z");
-    const compass_heading = document.getElementById("compass_heading");
-    const camera_angle = document.getElementById("camera_angle");
-    const geohash_7chars = document.getElementById("geohash_7chars");
+  camera.addEventListener("componentchanged", function (evt) {
+    console.log("HOLAS", evt);
+    switch (evt.detail.name) {
+      case "rotation":
+        //console.log('camera rotation changed', evt.detail.newData);
+        var compassRotation = camera.components["compass-rotation"],
+          lookControls = camera.components["look-controls"];
+        camera_angle.innerText = evt.detail.newData.y;
+        if (lookControls) {
+          yaw_angle.innerText = THREE.Math.radToDeg(
+            lookControls.yawObject.rotation.y
+          );
+        }
+        if (compassRotation) {
+          compass_heading.innerText = compassRotation.heading;
+        }
+        break;
+      case "position":
+        //console.log('camera position changed', evt.detail.newData);
+        var gpsPosition = camera.components["gps-position"];
+        camera_p_x.innerText = evt.detail.newData.x;
+        camera_p_z.innerText = evt.detail.newData.z;
+        if (gpsPosition) {
+          if (gpsPosition.crd) {
+            crd_longitude.innerText = gpsPosition.crd.longitude;
+            crd_latitude.innerText = gpsPosition.crd.latitude;
 
-    console.log("HOLAS", camera);
-
-    camera.addEventListener("componentchanged", function (evt) {
-      console.log("HOLAS", evt);
-      switch (evt.detail.name) {
-        case "rotation":
-          //console.log('camera rotation changed', evt.detail.newData);
-          var compassRotation = camera.components["compass-rotation"],
-            lookControls = camera.components["look-controls"];
-          camera_angle.innerText = evt.detail.newData.y;
-          if (lookControls) {
-            yaw_angle.innerText = THREE.Math.radToDeg(
-              lookControls.yawObject.rotation.y
+            var hash = Geohash.encode(
+              gpsPosition.crd.latitude,
+              gpsPosition.crd.longitude,
+              7
             );
+            geohash_7chars.innerText = hash;
           }
-          if (compassRotation) {
-            compass_heading.innerText = compassRotation.heading;
+          if (gpsPosition.zeroCrd) {
+            zero_crd_longitude.innerText = gpsPosition.zeroCrd.longitude;
+            zero_crd_latitude.innerText = gpsPosition.zeroCrd.latitude;
           }
-          break;
-        case "position":
-          //console.log('camera position changed', evt.detail.newData);
-          var gpsPosition = camera.components["gps-position"];
-          camera_p_x.innerText = evt.detail.newData.x;
-          camera_p_z.innerText = evt.detail.newData.z;
-          if (gpsPosition) {
-            if (gpsPosition.crd) {
-              crd_longitude.innerText = gpsPosition.crd.longitude;
-              crd_latitude.innerText = gpsPosition.crd.latitude;
+        }
 
-              var hash = Geohash.encode(
-                gpsPosition.crd.latitude,
-                gpsPosition.crd.longitude,
-                7
-              );
-              geohash_7chars.innerText = hash;
-            }
-            if (gpsPosition.zeroCrd) {
-              zero_crd_longitude.innerText = gpsPosition.zeroCrd.longitude;
-              zero_crd_latitude.innerText = gpsPosition.zeroCrd.latitude;
-            }
-          }
-
-          break;
-      }
-    });
-  };
+        break;
+    }
+  });
 };
+
+//DECLARAR LEMENTOS
+navigator.geolocation.getCurrentPosition(function (position) {
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+  let places = staticLoadPlaces();
+  renderPlaces(places);
+});
 
 function generateRandomInt(min, max) {
   const randomnum = Math.random() * (max - min);
